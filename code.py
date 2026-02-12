9b89d1849d4629048c7e34a714571c037ca4b57a
KakeguriMashoBayou <boyul@nycstudents.net>
2026-02-12 09:19:42 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,13 @@
 import random 
-nums: ["1","2","3","4","5","6","7","8","9","10"]
+nums = ["1","2","3","4","5","6","7","8","9","10"]
 guess = random.choice(nums)
 user = input("Guess the Number")
 
-while user ! guess 
-
+while user != "congratulations":
+    guess = input("guess a number from 1 to 10")
+    if guess = "5":
+        print(guess, "correct")
+    elif guess < "5":
+        print(guess, "incorrect")
+    elif guess => "5":
+        print(guess, "incorrect also")
