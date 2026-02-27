b94103df0d369edbf6cf8ef30714e1d7681312f8
KakeguriMashoBayou <boyul@nycstudents.net>
2026-02-27 09:24:15 -0500

    j

---
+++ code.py
@@ -0,0 +1,11 @@
+
+def occupied(x,y,t):
+    for i in range(x):
+        if(y[i] == "C" and t[i] == "C"):
+            found = 0
+            found +=1 
+    print(found)
+
+occupied (5, "CCCCC", "..CCC")
+occupied (7, "CCCCCCC", "C.C.C.C")
+         
